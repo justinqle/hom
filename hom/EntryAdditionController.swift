eb2173c8e8465e52b164ac48cd219775188182d1
Justin Le <justinqle@gmail.com>
2019-01-31 17:45:59 -0500

    Cancel dismisses EntryAdditionController

--- hom/EntryAdditionController.swift
+++ hom/EntryAdditionController.swift
@@ -20,6 +20,11 @@ class EntryAdditionController: UIViewController, UIScrollViewDelegate {
     }
     
     // MARK: - Navigation
+    
+    @IBAction func cancel(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
     }
