75edba1b6c8869c10e4d8dc932716bf63553ed9f
Dean Foster <Deanfoster45@gmail.com>
2019-02-08 01:29:17 -0500

    Working on pickers, tweaked SearchTextField

--- hom/EntryAdditionController.swift
+++ hom/EntryAdditionController.swift
@@ -21,20 +21,22 @@ class EntryAdditionController: UIViewController,
     @IBOutlet weak var patientInfoView: UIView!
     @IBOutlet weak var clinicTextField: BorderedTextField!
     @IBOutlet weak var genderStackView: BorderedStackView!
-    @IBOutlet weak var genderLabel: UILabel!
+
     @IBOutlet weak var ageTextField: BorderedTextField!
     @IBOutlet weak var diagnosisStackView: BorderedStackView!
-    @IBOutlet weak var diagnosisLabel: UILabel!
+
     @IBOutlet weak var dosageStackView: BorderedStackView!
     @IBOutlet weak var prescriptionTextField: SearchTextField!
-    @IBOutlet weak var dosageLabel: UILabel!
+
     @IBOutlet weak var notesTextView: PaddedTextView!
     @IBOutlet weak var creationLabel: PaddedLabel!
     @IBOutlet weak var deleteButtonView: UIView!
     @IBOutlet weak var deleteButton: UIButton!
     
     private let options = Options.shared
-    private var activeView: UIView?
+    private var activeInput: UIView?
+    private let pickerView = UIPickerView()
+    private var activePickerTrigger: UIStackView?
     
     // MARK: - Overriden Methods
     
@@ -53,7 +55,9 @@ class EntryAdditionController: UIViewController,
         providerTextField.delegate = self
         clinicTextField.delegate = self
         ageTextField.delegate = self
+        prescriptionTextField.delegate = self
         notesTextView.delegate = self
+        pickerView.delegate = self
         
         // Provider TextField borders
         providerTextField.layer.borderColor = UIColorCollection.greyDark.cgColor
@@ -69,7 +73,21 @@ class EntryAdditionController: UIViewController,
         
         // Customize SearchTextField instance for prescriptions
         prescriptionTextField.filterStrings(options.medicationList)
-        prescriptionTextField.inlineMode = true
+        prescriptionTextField.theme.font = UIFont.systemFont(ofSize: 18)
+        prescriptionTextField.itemSelectionHandler = { filteredResults, itemPosition in
+            let item = filteredResults[itemPosition]
+            self.prescriptionTextField.text = item.title
+            self.prescriptionTextField.resignFirstResponder()
+        }
+        
+        // Configure UIPickerView
+        pickerView.dataSource = self
+        pickerView.showsSelectionIndicator = true
+        
+        // Configure tap recorgnizers to UIStackViews that trigger pickers
+        genderStackView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(EntryAdditionController.pickerShouldTrigger(sender:))))
+        diagnosisStackView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(EntryAdditionController.pickerShouldTrigger(sender:))))
+        dosageStackView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(EntryAdditionController.pickerShouldTrigger(sender:))))
         
         // Dismiss the keyboard on tap of the content
         self.view.addGestureRecognizer(
@@ -78,22 +96,20 @@ class EntryAdditionController: UIViewController,
         // Register observers for keyboard notifications
         NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardDidShow),
                                                name: UIResponder.keyboardDidShowNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHide),
-                                               name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
     // MARK: - UITextFieldDelegate
     
     func textFieldDidBeginEditing(_ textField: UITextField) {
-        activeView = textField
+        activeInput = textField
     }
     
     func textFieldDidEndEditing(_ textField: UITextField) {
-        activeView = nil
+        activeInput = nil
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        activeView = nil
+        activeInput = nil
         textField.resignFirstResponder()
         return true
     }
@@ -101,21 +117,55 @@ class EntryAdditionController: UIViewController,
     // MARK: - UITextViewDelegate
     
     func textViewDidBeginEditing(_ textView: UITextView) {
-        activeView = textView
+        activeInput = textView
     }
     
     func textViewDidEndEditing(_ textView: UITextView) {
-        activeView = nil
+        activeInput = nil
+    }
+    
+    // MARK: - UIPickerViewDataSource
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        let trigger = activePickerTrigger!
+        
+        switch trigger {
+        case genderStackView:
+            return options.genderList.count
+        case diagnosisStackView:
+            return options.diagnosisList.count
+        case dosageStackView:
+            return options.dosageList.count
+        default:
+            fatalError("Invalid picker trigger: \(trigger)")
+        }
     }
     
     // MARK: - UIPickerViewDelegate
     
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 0
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        guard let trigger = activePickerTrigger else {
+            fatalError("Picker should not have been shown!")
+        }
+        
+        switch trigger {
+        case genderStackView:
+            return options.genderList[row]
+        case diagnosisStackView:
+            return options.diagnosisList[row]
+        case dosageStackView:
+            return options.dosageList[row]
+        default:
+            fatalError("Invalid picker trigger: \(trigger)")
+        }
     }
     
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return 0
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print("Row selected: \(row)")
     }
     
     // MARK: - Navigation
@@ -145,22 +195,27 @@ class EntryAdditionController: UIViewController,
         
         var difference: CGFloat = 0
         
-        if let textField = activeView {
-            // Calculate the difference between bottom of active view and top of keyboard
-            guard let fieldOrigin = textField.superview?.convert(textField.frame.origin, to: nil) else {
-                return
-            }
-            let bottomFieldY = fieldOrigin.y + textField.frame.height
-            difference = bottomFieldY - keyboardFrame.minY
-        }
-        else if let textView = activeView {
-            // Calculate the difference between bottom of active view and top of keyboard
+        if case let textView as UITextView = activeInput {
+            // Calculate the difference between bottom of active TextView and top of keyboard
             guard let viewOrigin = textView.superview?.convert(textView.frame.origin, to: nil) else {
                 return
             }
             // Difference + borderedStackView bottom margins set in IB
             let bottomFieldY = viewOrigin.y + textView.frame.height
             difference = bottomFieldY - keyboardFrame.minY + 20
+        }
+        else if var view = activeInput {
+            if case let superView as BorderedStackView = view.superview {
+                // If the superview is a BorderedStackView, use that instead
+                view = superView
+            }
+            
+            // Calculate the difference between bottom of active view (or its superview) and top of keyboard
+            guard let viewOrigin = view.superview?.convert(view.frame.origin, to: nil) else {
+                return
+            }
+            let bottomFieldY = viewOrigin.y + view.frame.height
+            difference = bottomFieldY - keyboardFrame.minY
         }
         
         if difference < 0 {
@@ -172,9 +227,18 @@ class EntryAdditionController: UIViewController,
             scrollView.setContentOffset(contentOffset, animated: true)
         }
     }
-
-    @objc private func keyboardWillHide(notification: Notification) {
-        // Reset the height and content offset if needed
+    
+    @objc private func pickerShouldTrigger(sender: UIGestureRecognizer) {
+        // Set the active trigger
+        activePickerTrigger = sender.view as? UIStackView
         
+        // Display the picker view
+        guard let innerField = activePickerTrigger?.subviews[1] as? UITextField else {
+            fatalError("Trigger does not have inner TextField!")
+        }
+        
+        innerField.inputView = pickerView
+        innerField.tintColor = UIColor.clear
+        innerField.becomeFirstResponder()
     }
 }
