b090905ec6c054f6ca23bf20b42a1616e3ff0bde
Dean Foster <Deanfoster45@gmail.com>
2019-02-13 15:40:21 -0500

    Built export scene

--- hom/Controllers/ExportViewController.swift
+++ hom/Controllers/ExportViewController.swift
@@ -11,7 +11,72 @@ import UIKit
 
 class ExportViewController: UIViewController {
     
+    // MARK: - Properties
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var providerLabel: UILabel!
+    @IBOutlet weak var rowLabel: UILabel!
+    @IBOutlet weak var sizeLabel: UILabel!
+    @IBOutlet weak var nameTextField: BorderedTextField!
+    
+    // MARK: - Overridden Methods
+    
     override func viewDidLoad() {
+        // Dismiss the keyboard on tap of the content
+        self.view.addGestureRecognizer(
+            UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:))))
         
+        // Register observers for keyboard notifications
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardDidShow),
+                                               name: UIResponder.keyboardDidShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHide),
+                                               name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func exportFile(_ sender: UIButton) {
+        
+    }
+    
+    // MARK: - Private Methods
+    
+    @objc private func keyboardDidShow(notification: Notification) {
+        guard let userInfo = notification.userInfo else {
+            print("No userInfo found")
+            return
+        }
+        
+        guard let keyboardSize = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else {
+            print("No keyboard frame found")
+            return
+        }
+        
+        let keyboardRect = keyboardSize.cgRectValue
+        
+        var difference: CGFloat = 0
+        
+        // Calculate the difference between bottom of active TextView and top of keyboard
+        guard let viewOrigin = nameTextField.superview?.convert(nameTextField.frame.origin, to: nil) else {
+            return
+        }
+        
+        // Calculate difference
+        let bottomFieldY = viewOrigin.y + nameTextField.frame.height
+        difference = bottomFieldY - keyboardRect.minY
+        
+        if difference < 0 {
+            // View is above keyboard - return
+            return
+        } else {
+            // View is hidden by keyboard, scroll up by difference
+            let contentOffset = CGPoint(x: 0, y: scrollView.contentOffset.y + difference)
+            scrollView.setContentOffset(contentOffset, animated: true)
+        }
+    }
+    
+    @objc private func keyboardWillHide(notification: Notification) {
+        // Reset the content offset
+        scrollView.setContentOffset(CGPoint(x: 0, y: 0), animated: true)
     }
 }
--- hom/Views/InsetTextField.swift
+++ hom/Views/InsetTextField.swift
@@ -12,11 +12,12 @@ import UIKit
 class InsetTextField: UITextField {
     
     // MARK: Properties
-    @IBInspectable var inset: CGFloat = 0
+    @IBInspectable var xInset: CGFloat = 0
+    @IBInspectable var yInset: CGFloat = 0
     
     // MARK: Drawing
     override func textRect(forBounds bounds: CGRect) -> CGRect {
-        return bounds.insetBy(dx: inset, dy: inset)
+        return bounds.insetBy(dx: xInset, dy: yInset)
     }
     
     override func editingRect(forBounds bounds: CGRect) -> CGRect {
