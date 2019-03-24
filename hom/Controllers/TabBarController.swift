9bd5172e36bf79c2769dd8ec377ce0e27e563289
Dean Foster <Deanfoster45@gmail.com>
2019-03-23 22:07:00 -0400

    Collapsed large title on export and settings scenes to better support smaller devices

--- hom/Controllers/TabBarController.swift
+++ hom/Controllers/TabBarController.swift
@@ -9,20 +9,20 @@
 import UIKit
 
 class TabBarController: UITabBarController, UITabBarControllerDelegate {
-    private var dataTable: DataTableController!
+    private var tableController: DataTableController!
     private var prevItemTitle: String!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.delegate = self
-        dataTable = ((viewControllers![0] as! UINavigationController).viewControllers.first as! DataTableController)
+        tableController = ((viewControllers![0] as! UINavigationController).viewControllers.first as! DataTableController)
         prevItemTitle = "Table"
     }
     
     override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
-        if prevItemTitle == "Table" && item.title == "Table" {
-            dataTable.tableView.scrollToRow(at: IndexPath(row: 0, section: 0), at: .top, animated: true)
+        if prevItemTitle == "Table" && item.title == "Table" && tableController.tableView.numberOfRows(inSection: 0) > 0 {
+            tableController.tableView.scrollToRow(at: IndexPath(row: 0, section: 0), at: .top, animated: true)
         }
         
         prevItemTitle = item.title!
