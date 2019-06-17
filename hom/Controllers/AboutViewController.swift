a35e6448235a32aa0cb20565cbeb6cde457a3301
Dean Foster <Deanfoster45@gmail.com>
2019-06-16 22:13:32 -0400

    Finish about scene UI layout

--- hom/Controllers/AboutViewController.swift
+++ hom/Controllers/AboutViewController.swift
@@ -13,22 +13,50 @@ class AboutViewController: UIViewController {
     //MARK: - Properties
     
     @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var contentView: UIView!
     @IBOutlet weak var appNameView: UIView!
     @IBOutlet weak var HOMTextView: UITextView!
+    @IBOutlet weak var devView: UIView!
+    @IBOutlet weak var deanCellView: UIView!
+    @IBOutlet weak var deanImageView: UIImageView!
+    @IBOutlet weak var justinImageView: UIImageView!
+    @IBOutlet weak var justinCellView: UIView!
     
     //MARK: - Overriden Methods
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        scrollView.contentSize = contentView.frame.size
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Border colors
+        // View border colors
         appNameView.layer.borderColor = UIColorCollection.greyDark.cgColor
         appNameView.layer.borderWidth = 1
+        devView.layer.borderColor = UIColorCollection.greyDark.cgColor
+        devView.layer.borderWidth = 1
         
-        // Configure HOM TextView
+        // TextViews
         HOMTextView.textContainer.lineFragmentPadding = 0
         HOMTextView.textContainerInset = .zero
         HOMTextView.textContainer.maximumNumberOfLines = 1
         HOMTextView.textContainer.lineBreakMode = .byTruncatingTail
+        
+        // ImageViews
+        deanImageView.layer.borderWidth = 1
+        deanImageView.layer.masksToBounds = false
+        deanImageView.layer.borderColor = UIColor.white.cgColor
+        deanImageView.layer.cornerRadius = deanImageView.frame.height / 2
+        deanImageView.clipsToBounds = true
+        
+        justinImageView.layer.borderWidth = 1
+        justinImageView.layer.masksToBounds = false
+        justinImageView.layer.borderColor = UIColor.white.cgColor
+        justinImageView.layer.cornerRadius = deanImageView.frame.height / 2
+        justinImageView.clipsToBounds = true
+        
     }
 }
